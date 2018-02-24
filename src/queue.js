0d894b3e81b06c27024562175f301cb368528bb9
Michael Cavalea <callmecavs@gmail.com>
2018-02-23 21:00:53 -0500

    start

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,31 @@
-const queue = () => {
+const queue = timeout => {
+  let callbackId
 
+  let tasks = []
+  let tasksIndex = 0
+  let tasksLength = 0
+
+  const add = task => {
+    tasks.push(task)
+    request()
+  }
+
+  const request = () => {
+    // if not already idle, request it
+    if (!callbackId) {
+      // use timeout option, defaulting to 1000 ms
+      window.requestIdleCallback(
+        handler,
+        { timeout: timeout || 1000 }
+      )
+    }
+  }
+
+  const flush = deadline => {
+    while (deadline.timeRemaining() > 0) {
+
+    }
+  }
 }
 
 export default queue
